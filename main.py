c2ef277d6944d902f0915397f755557e95f48697
JeanaeClark <jeclark@udel.edu>
2024-05-09 00:04:15 -0400

    Added ability to find song lyrics

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from bs4 import BeautifulSoup
+import requests
 
 cid = '0cad5195dfd04884b84ba221fe400bff'
 secret = '212724fad9f6414fb40a0d2caf57f4ee'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+def scrape_lyrics(artistname, songname):
+    # https://medium.com/swlh/how-to-leverage-spotify-api-genius-lyrics-for-data-science-tasks-in-python-c36cdfb55cf3
+    symbolString = "\"`~!@#$%^&*()_+-=[{]}\|;:',<.>/?"
+    artistname2 = str(artistname.replace(' ','-')).lower() if ' ' in artistname else str(artistname).lower()
+    songname2 = str(songname.replace(' ','-')).lower() if ' ' in songname else str(songname).lower()
+    songname2 = songname2.translate({ord(i): "-" for i in symbolString})
+    page = requests.get('https://www.songlyrics.com/'+ artistname2 + '/' + songname2 + '-lyrics/')
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics = html.find("p", id="songLyricsDiv")
+    return lyrics.get_text()
 
 songs = ["First", "Second"]
 for i in songs:
@@ -21,10 +34,9 @@ for i in songs:
             firstSong = r["tracks"]["items"][0]["id"]
         if i == "Second":
             secondSong = r["tracks"]["items"][0]["id"]
-    else:
+    elif response == "N":
         artist = input("\tEnter song artist: \n\t")
         q = "{} {}".format(song,artist)
-        print(q)
         r = sp.search(q, type = "track", limit=1, offset=0, market=None)
         print("\t{} by {}".format(r["tracks"]["items"][0]["name"],r["tracks"]["items"][0]["artists"][0]["name"]))
         print("\tSong Sample (if available): {}".format(r["tracks"]["items"][0]["preview_url"]))
@@ -38,3 +50,15 @@ for i in songs:
             print("Error")
             break
     print("\n\n")
+
+
+firstSongTitle = sp.track(firstSong)["name"]
+firstSongArtist = sp.track(firstSong)["artists"][0]["name"]
+firstSongLyrics = scrape_lyrics(firstSongArtist, firstSongTitle)
+
+
+secondSongTitle = sp.track(secondSong)["name"]
+secondSongArtist = sp.track(secondSong)["artists"][0]["name"]
+secondSongLyrics = scrape_lyrics(secondSongArtist, secondSongTitle)
+
+print(firstSongLyrics)
